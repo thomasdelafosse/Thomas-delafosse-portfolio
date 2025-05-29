5e41f7abdabeebdab687f99b150d58fa09a3d513
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-05-29 17:53:11 +0200

    Add Title on the header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Thomas Delafosse</title>
         <link
           rel="apple-touch-icon"
           sizes="180x180"
