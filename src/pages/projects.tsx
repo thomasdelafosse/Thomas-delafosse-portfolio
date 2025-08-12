100acaa320e313d7c75ff1d8ac13b0be8fcc87c3
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-08-12 17:53:28 +0200

    pages: add projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import MainSection from "@/components/main-section/MainSection";
+import BackgroundParticlesCanvas from "@/components/ui-background-pointillisme/BackgroundParticlesCanvas";
+import projectModelsData from "@/data/ProjectsInformation";
+import { useRouter } from "next/router";
+
+export default function ProjectsPage() {
+  const router = useRouter();
+  return (
+    <>
+      <div className="relative flex flex-col min-h-screen">
+        {/* Global background particles covering entire page including scroll */}
+        <BackgroundParticlesCanvas position="fixed" />
+        <button
+          onClick={() => router.push("/")}
+          className="fixed top-4 right-4 z-50 p-2 px-4 bg-black/80 hover:bg-black/40 text-white backdrop-blur border border-white/20 cursor-pointer"
+          aria-label="Show info"
+        >
+          ABOUT ME
+        </button>
+        <div className="fixed top-5 left-5 z-50">
+          <Image
+            src="/images/logoBlanc.png"
+            alt="Portfolio Logo"
+            width={80}
+            height={50}
+            priority
+          />
+        </div>
+        <MainSection projectModels={projectModelsData} />
+      </div>
+    </>
+  );
+}
