1c3eb3dc8ba4a67212d9a7620a74ce3c1f86f901
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-08-14 21:09:22 +0200

    chore(copy): tweak AboutMeSection text content and spacing

--- src/components/info-section/AboutMeSection.tsx
+++ src/components/info-section/AboutMeSection.tsx
@@ -2,7 +2,7 @@ const AboutMeSection = () => (
   <div className="flex flex-col items-center justify-center gap-4">
     <h1 className={`text-4xl font-bold  `}>Thomas Delafosse</h1>
     <span className="flex flex-col text-center items-center text-medium md:text-base whitespace-pre-line">
-      {`WEB DEVELOPER | REACT.JS | NEXT.JS | TYPESCRIPT. \n\n  I'm working on a portfolio in a video game style. \n Feel free to contact me for any question or collaboration opportunity.\n\n Tap anywhere to discover my projects. `}
+      {`WEB DEVELOPER | REACT.JS | NEXT.JS | TYPESCRIPT. \n\n  I'm working on a portfolio in a video game style using Three.js. \n Feel free to contact me for any question or collaboration opportunity.\n\n Tap anywhere to discover my projects. `}
     </span>
   </div>
 );
