a94bde463ea1aab5a2d82f57857cc3205868f6ec
Thomas Delafosse <bonjour@thomasdelafosse.com>
2025-08-15 20:34:35 +0200

    Add mobile "enter" button

--- src/components/info-section/InfoSection.tsx
+++ src/components/info-section/InfoSection.tsx
@@ -2,6 +2,7 @@ import AboutMeSection from "./AboutMeSection";
 import ContactSection from "./ContactSection";
 import BackgroundParticles from "../ui-background-pointillisme/backgroundParticles2d";
 import PointillismBackground from "@/components/ui-background-pointillisme/PointillismBackground";
+import Button from "@/components/ui/Button";
 
 interface InfoSectionTypes {
   onDiscoverProjects: () => void;
@@ -24,11 +25,12 @@ const InfoSection = ({
   return (
     <div
       style={pointillistBackgroundStyle}
-      className="fixed inset-0 z-40 flex flex-col items-center justify-center p-6 md:p-8 text-white cursor-pointer"
+      className="fixed inset-0 z-40 flex flex-col items-center justify-center p-6 md:p-8 text-white md:cursor-pointer"
     >
       {showPointillismBackground && <BackgroundParticles />}
       {showEnterMorph && (
         <PointillismBackground
+          className="hidden md:block"
           position="absolute"
           showBackground={false}
           showEnterMorph
@@ -36,11 +38,17 @@ const InfoSection = ({
         />
       )}
 
-      <div className="p-6 bg-black/70 absolute z-30 bottom-14">
+      <div className="p-6 bg-black/80 absolute z-30 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[90vw] max-w-md md:max-w-none md:w-auto md:bottom-14 md:top-auto md:translate-y-0 md:left-auto md:translate-x-0">
         <div className="flex flex-col md:flex-row gap-14">
           <AboutMeSection />
           <ContactSection />
         </div>
+      </div>
+
+      <div className="md:hidden flex justify-center items-center absolute z-30 bottom-10  ">
+        <Button onClick={onDiscoverProjects} variant="solid" aria-label="Enter">
+          ENTER
+        </Button>
       </div>
     </div>
   );
